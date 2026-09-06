ca9d44627adb1478cd7b7c2d5d6beb21da946234
Aditya <khude746@gmail.com>
2026-09-06 18:31:34 +0530

    Improved the notes app

--- Notes_application.py
+++ Notes_application.py
@@ -6,15 +6,14 @@ print("*"*150)
 
 names=[]
 def create():
-        
         global names
-        name=input("Enter the name of the file : ").strip()
+        name=input("Enter the name of the file: ").strip()
         names.append(name)
         f=open(f"{name}.txt","w")
         print("File Created Successfully")
         f.close()
 def open_file():
-        name=input("Enter the name of the file you want to open : ").strip()
+        name=input("Enter the name of the file you want to open: ").strip()
         if name in names:
                 f=open(f"{name}.txt","r")
                 print(f.read())
@@ -22,67 +21,43 @@ def open_file():
         else:
                 print("File Not Found")
 def edit():
-    
-    
-    name=input("Enter name of the file you want to edit : ").strip()
-    if name in names:
-            
-            f=open(f"{name}.txt","a")
-            print("Enter the text you want to write :")
-            while True:
-               text=input()
-               f.write(text)
-               f.write("\n")
-               while True:
-                       n=input("Enter Y to go to next line or S to save the file : ").strip()
-                       if n=="Y" or n=='y':
-                               break
-                       elif n=="S" or n=='s':
-                               print("File Saved Successfully")
-                               f.close()
-                               break
-                       else:
-                               print("Invalid Input")
-               if n=="Y" or n=="y":
-                       continue
-               else:
-                       break
-            
-                    
-               
-        
-            
-            
-    else:
-        
-            print("File Not Found")
-            
+        name=input("Enter the name of the file you want to edit: ").strip()
+        if name in names:
+                f=open(f"{name}.txt","a")
+                print("Enter the text you want to write: ")
+                print("(Note: - You write maximum 10,000 lines\n      - At the end after writing is over enter '#savefile' to stop writing and save the file.)")
+                for i in range(0,10000):
+                        text=input()
+                        if text.endswith("#savefile"):
+                                f.write(f"{text[:(len(text) -9)]}")
+                                print("File Saved Successfully")
+                                break
+                        else:
+                                f.write(text+"\n")
+                f.close()
+        else:
+                print("File Not Found")
 while True:
-    print("1. Create File")
-    print("2. Open File")
-    print("3. Edit File")
-    print("4. Exit")
+        print("1. Create File")
+        print("2. Open File")
+        print("3. Edit File")
+        print("4. Exit")
         
-    a=input("Enter your choice: ")
-    if int(a)==1:
-                
+        a=input("Enter your choice: ")
+        if int(a)==1:
                 create()
-                
-        
-    if int(a)==2:
-        
-        
-        open_file()
-        
-    if int(a)==3:
-        
-        
-        edit()
-    if int(a)==4:
-        print()
-        print("*"*150)
-        print()
-        print(f"{'THANK YOU':^150}")
-        print()
-        print("*"*150)
-        break
+        elif int(a)==2:
+                open_file()
+        elif int(a)==3:
+                edit()
+        elif int(a)==4:
+                print()
+                print("*"*150)
+                print()
+                print(f"{'THANK YOU':^150}")
+                print()
+                print("*"*150)
+                break
+        else:
+                print("Invalid Input")
+                       
